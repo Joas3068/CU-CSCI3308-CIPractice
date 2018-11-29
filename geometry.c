9d70aad899e435f4fb8993881ce4a0d2c331bb2f
Joas3068 <joas3068@colorado.edu>
2018-11-29 13:25:37 -0700

    Added area of triangle function

--- geometry.c
+++ geometry.c
@@ -48,6 +48,19 @@ bool coord_2d_eq(const coord_2d_t* a, const coord_2d_t* b){
 
 }
 
+double coord_2d_area_triangle(const coord_2d_t* a, const
+coord_2d_t* b, const coord_2d_t* c)
+{
+    if(!a || !b || !c)
+    {
+        DEBUG(__FILE__,__LINE__,__func__,"An input is NULL");
+        return -1;
+    }
+
+    return abs((a->x * (b->y - c->y) + b->x * (c->y - a->y) + c->x * (a->y - b->y))/2);
+
+}
+
 void coord_2d_midpoint(coord_2d_t* mid, const coord_2d_t* a, const coord_2d_t* b){
 
     /* Input Checks */
